e750cebc76acf1cb6c1213c13cf68660f85b3e63
Pedro_Magro <pedrohls.job@gmail.com>
2026-03-12 00:57:34 -0300

    utilizando filer reader e buffered reader

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -6,21 +8,38 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        File file = new File("Good morning!.txt");
-        Scanner sc = null;
-        try{
-            sc = new Scanner(file);
-            while(sc.hasNextLine()){
-                System.out.println(sc.nextLine());
+        String path = "C:\\Users\\pedro\\devProjetos\\FileReading\\Good morning!.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
             }
         }
-        catch (IOException e){
-            System.out.println("Error: " + e.getMessage());
+        catch(IOException e){
+            System.out.println("Erro: "+ e.getMessage());
         }
         finally{
-            if(sc != null){
-                sc.close();
+            try{
+                if(br != null){
+                    br.close();
+                }
+                if(fr != null){
+                    fr.close();
+                }
             }
+            catch(IOException e){
+                System.out.println("Erro: "+ e.getMessage());
+                e.printStackTrace();
+            }
+
         }
     }
 }
