14303c034e7f5b8b9c7fd59f23d5e196dbfbf0d0
Pedro_Magro <pedrohls.job@gmail.com>
2026-03-12 00:39:43 -0300

    Primeiro commit modulo de Manipulação de arquivos

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("Good morning!.txt");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally{
+            if(sc != null){
+                sc.close();
+            }
+        }
+    }
+}
